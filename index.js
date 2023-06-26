86b6732ce707048899912b5767a4f845b39f181b
Javier Esses <javiesses@hotmail.com>
2023-06-26 15:14:06 -0300

    Remove sh stuff and added HTML report

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// main.js
 const fs = require('fs');
 const path = require('path');
 
@@ -6,7 +7,34 @@ const contractsDir = './contracts';
 fs.readdir(contractsDir, (err, files) => {
   if (err) throw err;
 
-  let results = {};
+  let results = [];
+  let html = `
+    <style>
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+      th, td {
+        border: 1px solid #ddd;
+        padding: 8px;
+      }
+      tr:nth-child(even) {
+        background-color: #f2f2f2;
+      }
+      th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+      }
+    </style>
+    <table>
+      <tr>
+        <th>Contract Name</th>
+        <th>Number of Lines</th>
+      </tr>
+  `;
 
   files.forEach(file => {
     const filePath = path.join(contractsDir, file);
@@ -14,11 +42,17 @@ fs.readdir(contractsDir, (err, files) => {
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const lineCount = fileContent.split('\n').length;
 
-    results[file] = lineCount;
+    results.push({[file]: lineCount});
+
+    html += `<tr><td>${file}</td><td>${lineCount}</td></tr>`;
   });
 
-  console.log('Line counts:', results);
+  html += '</table>';
 
-  // Set output for other GitHub Actions steps
+  console.log('Line counts:', results);
+  console.log('HTML:', html);
+
+  // Set outputs for other GitHub Actions steps
   console.log(`::set-output name=lineCounts::${JSON.stringify(results)}`);
+  console.log(`::set-output name=html::${html}`);
 });
