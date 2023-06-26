9feddb11e00526c2e4c9871fca4cbc5260c83bc9
Javier Esses <javiesses@hotmail.com>
2023-06-26 14:03:23 -0300

    Count lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "Hello to $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+# count-lines.sh
+
+contracts_dir="./contracts"
+
+declare -A results
+
+for file in "$contracts_dir"/*; do
+  line_count=$(wc -l < "$file")
+  results[$file]=$line_count
+done
+
+echo "Line counts: ${results[@]}"
+
+echo "::set-output name=lineCounts::$(declare -p results)"
